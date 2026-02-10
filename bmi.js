ed50d7044b9a7687627233f3db087b7955b8b056
VikasMain_git91 <vp9198776636@gmail.com>
2026-02-10 17:10:32 +0530

    add condition

--- bmi.js
+++ bmi.js
@@ -1,16 +1,23 @@
 const form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    const height = parseInt(document.querySelector("#height").value)
-    const weight = parseInt(document.querySelector("#weight").value)
-    const results = document.querySelector(".results")
+    const height = parseInt(document.getElementById("height").value)
+    const weight = parseInt(document.getElementById("weight").value)
+    const result = document.querySelector(".results")
 
     if (height === "" || height < 0 || isNaN(height)) {
-        results.innerHTML = `Please give a valid height ${height}`;
+        result.innerHTML = `Please give a valid height ${height}`
     } else if (weight === "" || weight < 0 || isNaN(weight)) {
-        results.innerHTML = `Please give a valid weight ${weight}`;
+        result.innerHTML = `Please give a valid weight ${weight}`
     } else {
-        const bmIvalue = ((weight / (height * height / 10000)) / 1000).toFixed(2);
-        results.innerHTML = `<span>${bmIvalue}</span>`;
+        const bmiValue = (weight / ((height * height) / 10000)).toFixed(2);
+        result.innerHTML = `${bmiValue}`
+        if (bmiValue <= 18.8) {
+            result.innerHTML = `${bmiValue} (Under weight) `
+        } else if (bmiValue > 18.6 && bmiValue <= 24.9) {
+            result.innerHTML = `${bmiValue} (Normal Range) `
+        } else {
+            result.innerHTML = `${bmiValue} (Over weight) `
+        }
     }
-});
+})
